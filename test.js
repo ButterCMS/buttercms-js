e15ebb25a52b0c996489a901ed765210f0ba8f46
Camilo Reyes <reyesc@gmail.com>
2017-08-05 18:49:10 -0500

    Auto retry exception handling with polly-js lib

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 var butter = require('./lib/butter')('b60a008584313ed21803780bc9208557b3b49fbb');
+//var butter = require('./lib/butter')('b60a008584313ed21803780bc9208557b3b49fbb', false, 1); // Network timeout
 
 // butter.content.retrieve(["abis_team"], {locale: 'es'})
 //   .then(function(response) {
@@ -28,10 +29,21 @@ var butter = require('./lib/butter')('b60a008584313ed21803780bc9208557b3b49fbb')
 //     console.log(response)
 //   });
 
-
 // butter.content.retrieve(["landing_pages"])
 //   .then(function(response) {
 //     console.log(response.data.data)
 //   }).catch(function(response) {
 //     console.log(response)
+//   });
+
+// butter.post.retrieve("directly-import-pdfs-into-your-blog-post")
+//   .handle(function(error) {
+//     return error.timeout;
+//   })
+//   .executeWithAutoRetry(3)
+//   .then(function(response) {
+//     console.log(response);
+//   })
+//   .catch(function(response) {
+//     console.log(response);
 //   });
