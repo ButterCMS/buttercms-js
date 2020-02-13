4c2474b6ca8f7e81c051ed023019800e82fb4d8f
Damien CORLER <contact@corler.pro>
2020-02-13 18:47:45 +0100

    Remove unnecessary module declaration

--- lib/butter.d.ts
+++ lib/butter.d.ts
@@ -1,76 +1,74 @@
 import { AxiosInstance } from "axios";
 
-declare module "buttercms" {
-  export namespace Butter {
-    interface Response {
-      data?: any;
-      status?: number;
-      statusText?: string;
-      headers?: any;
-      config?: any;
-    }
-
-    interface PostMethods {
-      retrieve(slug: string, params?: any): Promise<Response>;
-      list(params?: any): Promise<Response>;
-      search(query: string, params?: any): Promise<Response>;
-    }
-
-    interface CategoryMethods {
-      retrieve(slug: string, params?: any): Promise<Response>;
-      list(params?: any): Promise<Response>;
-    }
-
-    interface TagMethods {
-      retrieve(slug: string, params?: any): Promise<Response>;
-      list(params?: any): Promise<Response>;
-    }
-
-    interface AuthorMethods {
-      retrieve(slug: string, params?: any): Promise<Response>;
-      list(params?: any): Promise<Response>;
-    }
-
-    interface FeedMethods {
-      retrieve(slug: string, params?: any): Promise<Response>;
-    }
-
-    interface PageMethods {
-      retrieve(
-        page_type: string,
-        page_slug: string,
-        params?: any
-      ): Promise<Response>;
-      list(page_type: string, params?: any): Promise<Response>;
-    }
-
-    interface ContentMethods {
-      retrieve(keys: Array<string>, params?: any): Promise<Response>;
-    }
+export namespace Butter {
+  interface Response {
+    data?: any;
+    status?: number;
+    statusText?: string;
+    headers?: any;
+    config?: any;
   }
 
-  export class ButterStatic {
-    post: Butter.PostMethods;
-    category: Butter.CategoryMethods;
-    tag: Butter.TagMethods;
-    author: Butter.AuthorMethods;
-    feed: Butter.FeedMethods;
-    page: Butter.PageMethods;
-    content: Butter.ContentMethods;
-    constructor(
-      apiToken: string,
-      testMode?: boolean,
-      timeout?: number,
-      axiosHook?: (axios: AxiosInstance) => unknown
-    );
+  interface PostMethods {
+    retrieve(slug: string, params?: any): Promise<Response>;
+    list(params?: any): Promise<Response>;
+    search(query: string, params?: any): Promise<Response>;
   }
 
-  export const Butter: (
+  interface CategoryMethods {
+    retrieve(slug: string, params?: any): Promise<Response>;
+    list(params?: any): Promise<Response>;
+  }
+
+  interface TagMethods {
+    retrieve(slug: string, params?: any): Promise<Response>;
+    list(params?: any): Promise<Response>;
+  }
+
+  interface AuthorMethods {
+    retrieve(slug: string, params?: any): Promise<Response>;
+    list(params?: any): Promise<Response>;
+  }
+
+  interface FeedMethods {
+    retrieve(slug: string, params?: any): Promise<Response>;
+  }
+
+  interface PageMethods {
+    retrieve(
+      page_type: string,
+      page_slug: string,
+      params?: any
+    ): Promise<Response>;
+    list(page_type: string, params?: any): Promise<Response>;
+  }
+
+  interface ContentMethods {
+    retrieve(keys: Array<string>, params?: any): Promise<Response>;
+  }
+}
+
+export class ButterStatic {
+  post: Butter.PostMethods;
+  category: Butter.CategoryMethods;
+  tag: Butter.TagMethods;
+  author: Butter.AuthorMethods;
+  feed: Butter.FeedMethods;
+  page: Butter.PageMethods;
+  content: Butter.ContentMethods;
+  constructor(
     apiToken: string,
     testMode?: boolean,
     timeout?: number,
     axiosHook?: (axios: AxiosInstance) => unknown
-  ) => ButterStatic;
-
-  export default Butter;
+  );
 }
+
+export const Butter: (
+  apiToken: string,
+  testMode?: boolean,
+  timeout?: number,
+  axiosHook?: (axios: AxiosInstance) => unknown
+) => ButterStatic;
+
+export default Butter;
