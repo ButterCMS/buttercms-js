c30bff68aade1b17b64d9d7ef70d3f286576d56c
Nick McBurney <nick@nickmcburney.co.uk>
2023-03-29 20:47:58 +0100

    enable unit test coverage and set thresholds

--- jest.config.ts
+++ jest.config.ts
@@ -10,6 +10,16 @@ const config: Config = {
   transformIgnorePatterns: ["node_modules/(?!isomorphic-unfetch)"],
   automock: false,
   resetMocks: false,
+  collectCoverage: true,
+  coverageProvider: 'v8',
+  coverageThreshold: {
+    global: {
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95,
+    },
+  },
   setupFiles: [
     "./jest.setup.ts"
   ]
